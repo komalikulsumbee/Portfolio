a2e618d17815be4aef1ebf192cb9148346171e60
komalikulsumbee <120927828+komalikulsumbee@users.noreply.github.com>
2024-11-20 13:31:49 +0530

    Add files via upload

---
+++ Portfolio/port.js
@@ -0,0 +1,30 @@
+// let document=document.querySelectorAll('section');
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.nav');
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// let navlists=document.querySelectorAll('header nav a ')
+window.onscroll =() =>{
+//     section.forEach(sec =>{
+//         let top=window.scrollY;
+//         let offset=sec.offsetTop - 100;
+//         let height =sec.offsetHeight;
+//         let id =sec.getAttribute('id');
+//         if(top >= offset && offset + height){
+//             navLinks.forEach(links =>{
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+//             });
+// sec.classList.add('show-animate');
+//         }
+// else{
+//     sec.classList.remove('show-animate')
+// }
+//     });
+    let header= document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
